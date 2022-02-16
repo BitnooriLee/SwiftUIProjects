a71a17607e3f7b406793d7e36ec385fe72a2b11c
Bitnoori Lee <bitnoori.lee@gmail.com>
2022-02-16 21:35:55 +0100

    First commit for Guess the Flag

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Bitnoori Lee on 2022-02-15.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
