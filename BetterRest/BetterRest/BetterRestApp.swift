c8b4eea8235195e5fea1d6d54d790bf6ca76c6f5
Bitnoori Lee <bitnoori.lee@gmail.com>
2022-02-19 14:24:51 +0100

    inital commit for BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Bitnoori Lee on 2022-02-18.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
