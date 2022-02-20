8c9d941add89cc6416ca7da2e71a714817327a67
Bitnoori Lee <bitnoori.lee@gmail.com>
2022-02-20 07:22:36 +0100

    Initial commit for WordScramble

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Bitnoori Lee on 2022-02-19.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
