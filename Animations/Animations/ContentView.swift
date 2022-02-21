0df07f9ab7aacdda5f839d303a8888a37a906d03
bitnoori <bitnoori.lee@gmail.com>
2022-02-21 14:55:52 +0100

    Add gestures and animation with viewModifier

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -7,11 +7,112 @@
 
 import SwiftUI
 
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition{
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading), identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
 struct ContentView: View {
-    @State private var animationAmount = 1.0
-    @State private var animationAmount2 = 0.0
+ //  @State private var animationAmount = 1.0
+ //   @State private var animationAmount2 = 0.0
+
+ //     @State private var enabled = false
+ //     @State private var dragAmount = CGSize.zero
+    @State private var isShowingRed = false
+    
+    let letters = Array("Hello, SwiftUI")
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
+    
+    var body: some View {
+        
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            }
+        }
+        .onTapGesture {
+            withAnimation{
+                isShowingRed.toggle()
+            }
+        }
+        
+        HStack(spacing: 0){
+            ForEach(0..<letters.count) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(
+                        .default.delay(Double(num) / 20),
+                        value: dragAmount)
+            }
+        }
+        .gesture(
+         DragGesture()
+            .onChanged { dragAmount = $0.translation }
+            .onEnded { _ in
+                dragAmount = .zero
+                enabled.toggle()
+            }
+         )
+    
+    }
+ // Gesture(Drag)
+ /*       LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged{ dragAmount = $0.translation }
+                    .onEnded { _ in
+                        withAnimation {
+                        dragAmount = .zero }
+                    
+                }
+            
+            )//.animation(.spring(), value: dragAmount)
+*/
+        
+ // Button with Animation
+    /*
     var body: some View {
         VStack{
+        Button("Tap me"){
+            enabled.toggle()
+        
+        }
+        .frame(width: 200, height: 200)
+        .background(enabled ? .blue : .red)
+        .animation(.default, value: enabled)
+        .foregroundColor(.white)
+        .clipShape(RoundedRectangle(cornerRadius:  enabled ? 60 : 0))
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
+        
+            
         Button ("Tap me") {
             withAnimation(.interpolatingSpring(stiffness: 50, damping: 1)) {
                 animationAmount2 += 360
@@ -49,6 +150,7 @@ struct ContentView: View {
         }
         
     }
+    } */
 }
 
 struct ContentView_Previews: PreviewProvider {
