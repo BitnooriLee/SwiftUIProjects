d5f809b0e2160a032f1927770ee819f693ec0324
Bitnoori Lee <bitnoori.lee@gmail.com>
2022-03-12 19:56:58 +0100

    First commit for Bucket List

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Bitnoori Lee on 2022-03-11.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketList/BucketList/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Bitnoori Lee on 2022-03-11.
+//
+import LocalAuthentication
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var isUnlocked = false
+    
+    var body: some View {
+        VStack {
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            
+            let reason = "We need to unlock your data"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    isUnlocked = true
+                } else {
+                    
+                }
+            }
+        } else {
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
