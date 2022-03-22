fe4893be32fb41dfb0f120e9776f807dbe6815eb
bitnoori <bitnoori.lee@gmail.com>
2022-03-22 18:06:06 +0100

    Add QR code

--- HotProspects/HotProspects/ProspectView.swift
+++ HotProspects/HotProspects/ProspectView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Bitnoori Lee on 2022-03-21.
 //
-
+import CodeScanner
 import SwiftUI
 
 struct ProspectView: View {
@@ -13,31 +13,49 @@ struct ProspectView: View {
     }
     
     @EnvironmentObject var prospects: Prospects
+    @State private var isShowingScanner = false
+    
     let filter: FilterType
     
     var body: some View {
         NavigationView{
             List{
-                ForEach(filteredProspects){prospect in
+                ForEach(filteredProspects){ prospect in
                     VStack(alignment: .leading) {
                         Text(prospect.name)
                             .font(.headline)
                         Text(prospect.emailAddress)
                             .foregroundColor(.secondary)
                     }
+                    .swipeActions {
+                        if prospect.isContacted {
+                            Button {
+                                prospects.toggle(prospect)
+                            } label: {
+                                Label("Mark Uncontacted", systemImage: "person.crop.circle.badge.xmark")
+                            }
+                            .tint(.blue)
+                        } else {
+                            Button {
+                                prospects.toggle(prospect)
+                            } label: {
+                                Label("Mark Contacted", systemImage: "person.crop.circle.fill.badge.checkmark")
+                            }
+                            .tint(.green)
+                        }
+                    }
                 }
             }
                 .navigationTitle(title)
                 .toolbar {
                     Button {
-                        let prospect = Prospect()
-                        prospect.name = "Paul Hudson"
-                        prospect.emailAddress =
-                        "paul@hackingwithswift.com"
-                        prospects.people.append(prospect)
+                       isShowingScanner = true
                     } label: {
                         Label("Scan", systemImage:  "qrcode.viewfinder")
                     }
+                }
+                .sheet(isPresented: $isShowingScanner) {
+                    CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaul@hackingwithswift.com", completion: handleScan)
                 }
         }
     }
@@ -63,6 +81,22 @@ struct ProspectView: View {
             return prospects.people.filter {
                 !$0.isContacted
             }
+        }
+    }
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        isShowingScanner = false
+        
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+            
+            let person = Prospect()
+            person.name = details[0]
+            person.emailAddress = details[1]
+            prospects.people.append(person)
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
         }
     }
 }
