b01247c0eabe4d0afaa2d2216bce91cca13f8a06
bitnoori <bitnoori.lee@gmail.com>
2022-03-28 12:25:20 +0200

    First commit for Flashzilla

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Bitnoori Lee on 2022-03-25.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
